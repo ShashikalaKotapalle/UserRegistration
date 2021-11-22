75a6ab121cd9676ed6ada52e53ab1158c94f79cd
Shashi <Shashikala.Kotapalle@drs.co.uk>
2021-11-22 16:44:07 +0000

    Committed RequestDto and ResponseDto

---
+++ src/UserRegistration.Web.Api/Controllers/User/RequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace UserRegistration.Web.Api.Controllers.User
+{
+	public class RequestDto
+	{
+		public string UserName { get; set; }
+	}
+}
---
+++ src/UserRegistration.Web.Api/Controllers/User/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegistration.Web.Api.Controllers.User
+{
+	public class ResponseDto
+	{
+		public int Id { get; set; }
+
+		public string UserName { get; set; }
+	}
+}
